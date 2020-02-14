813dbb5523f58b8d7473a6d9537f712e42a05af5
jillianKhoo <jkhoo@usc.edu>
2020-02-14 11:56:03 -0800

    surpise <3

---
+++ Knowledge/Knowledge Database/Computer Science Notes/Python/Practice Code/hmm.py
@@ -0,0 +1,6 @@
+class hmmm:
+    x = 0.01
+    for y in xrange(25):
+        x *= 2
+
+    print x
---
+++ Knowledge/Knowledge Database/Computer Science Notes/Python/Practice Code/test2.java
@@ -0,0 +1,9 @@
+public class test2 {
+    static int anum = 2;
+    public static void main(String[] args) {
+    }
+
+    public static void ameth() {
+        System.out.println("YAYYYYYY");
+    }
+}
